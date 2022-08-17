08c12b8209237b143132086056efa2a873d16cf6
yjason12 <jasonl121415@gmail.com>
2022-08-17 16:00:49 -0700

    neighbors hard code

--- models/JunqiBoard.ts
+++ models/JunqiBoard.ts
@@ -7,7 +7,6 @@ export class JunqiBoard implements IBoard {
 
     board: Tile[][];
 
-
     constructor(){
         this.board = this.getNewBoard();
     }
@@ -20,12 +19,82 @@ export class JunqiBoard implements IBoard {
     private getNewBoard(): Tile[][]{
         //new Tile(new Piece(pieceType, player), tileType)
 
-        var a1 = new Tile(new Piece( , 0), 1);
+
+        //player 0
+        var a1 = new Tile(new Piece(11 , 0), 1);
+        var a2 = new Tile(new Piece(1, 0), 1);
+        var a3 = new Tile(new Piece(4, 0), 1);
+        var a4 = new Tile(new Piece(2, 0), 1);
+        var a5 = new Tile(new Piece(6, 0), 1);
+        var a6 = new Tile(new Piece(3, 0), 1);
+
+        var b1 = new Tile(new Piece(0, 0), 4);
+        var b2 = new Tile(new Piece(10, 0), 1);
+        var b3 = new Tile(new Piece(-1, -1), 2);
+        var b4 = new Tile(new Piece(7, 0), 1);
+        var b5 = new Tile(new Piece(-1, -1), 2);
+        var b6 = new Tile(new Piece(8, 0), 1);
+
+        var c1 = new Tile(new Piece(10, 0), 1);
+        var c2 = new Tile(new Piece(10, 0), 1);
+        var c3 = new Tile(new Piece(2, 0), 1);
+        var c4 = new Tile(new Piece(-1, -1), 2);
+        var c5 = new Tile(new Piece(5, 0), 1);
+        var c6 = new Tile(new Piece(3, 0), 1);
+        
+        var d1 = new Tile(new Piece(11, 0), 4);
+        var d2 = new Tile(new Piece(10, 0), 1);
+        var d3 = new Tile(new Piece(-1, -1), 2);
+        var d4 = new Tile(new Piece(7, 0), 1);
+        var d5 = new Tile(new Piece(-1, 0), 2);
+        var d6 = new Tile(new Piece(8, 0), 1);
+        
+        var e1 = new Tile(new Piece(4 , 0), 1);
+        var e2 = new Tile(new Piece(1, 0), 1);
+        var e3 = new Tile(new Piece(5, 0), 1);
+        var e4 = new Tile(new Piece(2, 0), 1);
+        var e5 = new Tile(new Piece(6, 0), 1);
+        var e6 = new Tile(new Piece(3, 0), 1);
 
 
+        // player 1
+        var a7 = new Tile(new Piece(3, 1), 1);
+        var a8 = new Tile(new Piece(6, 1), 1);
+        var a9 = new Tile(new Piece(2, 1), 1);
+        var a10 = new Tile(new Piece(4, 1), 1);
+        var a11 = new Tile(new Piece(1, 1), 1);
+        var a12 = new Tile(new Piece(11, 1), 1);
+
+        var b7 = new Tile(new Piece(8, 1), 1);
+        var b8 = new Tile(new Piece(-1, 1), 2);
+        var b9 = new Tile(new Piece(7, 1), 1);
+        var b10 = new Tile(new Piece(-1, 1), 2);
+        var b11 = new Tile(new Piece(10, 1), 1);
+        var b12 = new Tile(new Piece(0, 1), 4);
+        
+        var c7 = new Tile(new Piece(3, 1), 1);
+        var c8 = new Tile(new Piece(5, 1), 1);
+        var c9 = new Tile(new Piece(-1, 1), 2);
+        var c10 = new Tile(new Piece(2, 1), 1);
+        var c11 = new Tile(new Piece(10, 1), 1);
+        var c12 = new Tile(new Piece(10, 1), 1);
+
+        var d7 = new Tile(new Piece(8, 1), 1);
+        var d8 = new Tile(new Piece(-1, 1), 2);
+        var d9 = new Tile(new Piece(7, 1), 1);
+        var d10 = new Tile(new Piece(-1, 1), 2);
+        var d11 = new Tile(new Piece(10, 1), 1);
+        var d12 = new Tile(new Piece(11, 1), 4);
+        
+        var e7 = new Tile(new Piece(3, 1), 1);
+        var e8 = new Tile(new Piece(6, 1), 1);
+        var e9 = new Tile(new Piece(2, 1), 1);
+        var e10 = new Tile(new Piece(5, 1), 1);
+        var e11 = new Tile(new Piece(1, 1), 1);
+        var e12 = new Tile(new Piece(4, 1), 1);
 
         var newBoard =
-        [ [a13, b13, c13, d13, e13 ],
+        [
           [a12, b12, c12, d12, e12 ],
           [a11, b11, c11, d11, e11 ],
           [a10, b10, c10, d10, e10 ],
@@ -39,9 +108,146 @@ export class JunqiBoard implements IBoard {
           [a2, b2, c2, d2, e2 ],
           [a1, b1, c1, d1, e1 ] ];
         
-          
+
         a1.setRoadNeighbors([ a2, b1 ]);
         a1.setRailRoadNeighbors([]);  
+        b1.setRoadNeighbors([a1, c1, b2]);
+        b1.setRailRoadNeighbors([]);  
+        c1.setRoadNeighbors([b1, d1, c2]);
+        c1.setRailRoadNeighbors([]);  
+        d1.setRoadNeighbors([c1, e1, d2]);
+        d1.setRailRoadNeighbors([]);  
+        e1.setRoadNeighbors([d1, e2]);
+        e1.setRailRoadNeighbors([]);  
+
+
+        a2.setRoadNeighbors([ a1, b3 ]);
+        a2.setRailRoadNeighbors([a3, b2]);  
+        b2.setRoadNeighbors([b1, b3]);
+        b2.setRailRoadNeighbors([a2, c2]);  
+        c2.setRoadNeighbors([c1, b3, c3, d3]);
+        c2.setRailRoadNeighbors([b2, d2]);  
+        d2.setRoadNeighbors([d1, d3]);
+        d2.setRailRoadNeighbors([c2, e2]);  
+        e2.setRoadNeighbors([e1]);
+        e2.setRailRoadNeighbors([d2,e3]);  
+
+        
+        a3.setRoadNeighbors([ b3 ]);
+        a3.setRailRoadNeighbors([a4, a2]);  
+        b3.setRoadNeighbors([a4, b4, c4, a3, c3, a2, b2, c2]);
+        b3.setRailRoadNeighbors([]);  
+        c3.setRoadNeighbors([c4, b3, d3, c2]);
+        c3.setRailRoadNeighbors([]);  
+        d3.setRoadNeighbors([c4, d4, e4, c3, e3, c2, d2, e2]);
+        d3.setRailRoadNeighbors([]);  
+        e3.setRoadNeighbors([d3]);
+        e3.setRailRoadNeighbors([e4, e2]);  
+           
+
+        a4.setRoadNeighbors([b5, b4, b3]);
+        a4.setRailRoadNeighbors([a5, a3]);  
+        b4.setRoadNeighbors([b5, a4, c4, b3]);
+        b4.setRailRoadNeighbors([]);  
+        c4.setRoadNeighbors([b5, c5, d5, b4, d4, b3, c3, d3]);
+        c4.setRailRoadNeighbors([]);  
+        d4.setRoadNeighbors([d5, c4, e4, d3]);
+        d4.setRailRoadNeighbors([]);  
+        e4.setRoadNeighbors([d5, d4, d3]);
+        e4.setRailRoadNeighbors([e5, e3]);  
+
+        
+        a5.setRoadNeighbors([b5]);
+        a5.setRailRoadNeighbors([a6, a4]);  
+        b5.setRoadNeighbors([a6, b6, c6, a5, c5, a4, b4, c4]);
+        b5.setRailRoadNeighbors([]);  
+        c5.setRoadNeighbors([c6, b5, d5, c3]);
+        c5.setRailRoadNeighbors([]);  
+        d5.setRoadNeighbors([c6, d6, e6, c5, e5, c4, d4, e4]);
+        d5.setRailRoadNeighbors([]);  
+        e5.setRoadNeighbors([d5]);
+        e5.setRailRoadNeighbors([e6, e4]);  
+
+
+        a6.setRoadNeighbors([b5]);
+        a6.setRailRoadNeighbors([a5, a7, b6]);  
+        b6.setRoadNeighbors([b5]);
+        b6.setRailRoadNeighbors([a6, c6]);  
+        c6.setRoadNeighbors([c5]);
+        c6.setRailRoadNeighbors([b6, c7, d6]);  
+        d6.setRoadNeighbors([d5]);
+        d6.setRailRoadNeighbors([e6, c6]);  
+        e6.setRoadNeighbors([d5]);
+        e6.setRailRoadNeighbors([e7, e5, d6]); 
+
+        
+        a7.setRoadNeighbors([]);
+        a7.setRailRoadNeighbors([]);  
+        b7.setRoadNeighbors([]);
+        b7.setRailRoadNeighbors([]);  
+        c7.setRoadNeighbors([]);
+        c7.setRailRoadNeighbors([]);  
+        d7.setRoadNeighbors([]);
+        d7.setRailRoadNeighbors([]);  
+        e7.setRoadNeighbors([]);
+        e7.setRailRoadNeighbors([]); 
+
+        a8.setRoadNeighbors([]);
+        a8.setRailRoadNeighbors([]);  
+        b8.setRoadNeighbors([]);
+        b8.setRailRoadNeighbors([]);  
+        c8.setRoadNeighbors([]);
+        c8.setRailRoadNeighbors([]);  
+        d8.setRoadNeighbors([]);
+        d6.setRailRoadNeighbors([]);  
+        e8.setRoadNeighbors([]);
+        e8.setRailRoadNeighbors([]); 
+
+        a9.setRoadNeighbors([]);
+        a9.setRailRoadNeighbors([]);  
+        b9.setRoadNeighbors([]);
+        b9.setRailRoadNeighbors([]);  
+        c9.setRoadNeighbors([]);
+        c9.setRailRoadNeighbors([]);  
+        d9.setRoadNeighbors([]);
+        d9.setRailRoadNeighbors([]);  
+        e9.setRoadNeighbors([]);
+        e9.setRailRoadNeighbors([]); 
+
+        a10.setRoadNeighbors([]);
+        a10.setRailRoadNeighbors([]);  
+        b10.setRoadNeighbors([]);
+        b10.setRailRoadNeighbors([]);  
+        c10.setRoadNeighbors([]);
+        c10.setRailRoadNeighbors([]);  
+        d10.setRoadNeighbors([]);
+        d10.setRailRoadNeighbors([]);  
+        e10.setRoadNeighbors([]);
+        e10.setRailRoadNeighbors([e11, e9]); 
+        
+        a11.setRoadNeighbors([a12, a10, b10]);
+        a11.setRailRoadNeighbors([a10, b11]);  
+        b11.setRoadNeighbors([b12, b10]);
+        b11.setRailRoadNeighbors([a11, c11]);  
+        c11.setRoadNeighbors([b10, d10]);
+        c11.setRailRoadNeighbors([b11, d11]);  
+        d11.setRoadNeighbors([d12, d10]);
+        d11.setRailRoadNeighbors([c11, e11]);  
+        e11.setRoadNeighbors([d10, e12]);
+        e11.setRailRoadNeighbors([d11, e10]); 
+
+        a12.setRoadNeighbors([a11, b12]);
+        a12.setRailRoadNeighbors([]);  
+        b12.setRoadNeighbors([a12, c12, b11]);
+        b12.setRailRoadNeighbors([]);  
+        c12.setRoadNeighbors([b12, d12, c11]);
+        c12.setRailRoadNeighbors([]);  
+        d12.setRoadNeighbors([c12, e12, d11]);
+        d12.setRailRoadNeighbors([]);  
+        e12.setRoadNeighbors([d12, e11]);
+        e12.setRailRoadNeighbors([]); 
+        
+        
 
 
 
