67af82515e284d40a8cb3227322a25af9c24c0b7
Michael Chen <mdchenmail@gmail.com>
2022-08-24 13:36:55 -0700

    untested db functinos

--- api/controllers/JunqiGameDBController.ts
+++ api/controllers/JunqiGameDBController.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { IBoard } from "../../models/interfaces/IBoard";
 import JunqiGame from "../../models/interfaces/IJunqiGame";
 import { JunqiBoard } from "../../models/JunqiBoard";
 
@@ -21,15 +22,75 @@ const createJunqiGame = (roomName : string, connectedSockets : Set<string>) => {
     return jg.save();
 };
 
+const updateJunqiGame = async (board : IBoard, gameName : string) => {
+    if (await JunqiGame.exists({ name : gameName})) {
+        console.log(`Updating game ${gameName}`);
+        JunqiGame.updateOne({name : gameName}, { board : board });
+    } else {
+        console.log(`Game ${gameName} does not exist`)
+    }
+    return;
+}
+
+const setReadyJunqiGame = async (playerName : string, gameName : string, state : boolean) => {
+    if (await JunqiGame.exists({ name : gameName})) {
+        console.log(`Readying player: ${playerName} in JunqiGame: ${gameName}`);
+        const game = await JunqiGame.find({ name : gameName });
+        if (game[0].players.includes(playerName)){
+            game[0].ready.set(playerName, state);
+        }
+        game[0].save();
+    } else {
+        console.log(`Game ${gameName} does not exist`)
+    }
+}
+
+const startJunqiGame = async (gameName : string) => {
+    if (await JunqiGame.exists({ name : gameName})) {
+        console.log(`Starting JunqiGame: ${gameName}`);
+        const game = await JunqiGame.find({ name : gameName });
+        
+        game[0].started = true;
+        game[0].save();
+    } else {
+        console.log(`Game ${gameName} does not exist`);
+    }
+}
+
+const addPlayerToJunqiGame = async (playerName : string, gameName : string) => {
+    if (await JunqiGame.exists({ name : gameName})) {
+        console.log(`Starting JunqiGame: ${gameName}`);
+        const game = await JunqiGame.find({ name : gameName });
+        for (let i = 0; i < 2; i++) {
+            if (game[0].players[i] === "") {
+                game[0].players[i] = playerName;
+                await game[0].save();
+                return;
+            }
+        }
+        console.log(`Game ${gameName} is full`);
+    } else {
+        console.log(`Game ${gameName} does not exist`);
+    }
+    return;
+}
+
 const deletePlayerFromJunqiGame = async (playerName : string) => {
     try{
         console.log(`Deleting player ${playerName} from JunqiGame`);
         const game = await JunqiGame.find({ players : playerName });
+        /*
         for (let i = 0; i < 2; i++) {
             if (game[0].players[i] === playerName) {
                 game[0].players[i] = "";
                 await game[0].save();
             }
+        }
+        */
+        var ind = game[0].players.indexOf(playerName);
+        if (ind !== -1) {
+            game[0].players[ind] = "";
+            game[0].save();
         }
         if (game[0].players[0] === "" && game[0].players[1] === ""){
             deleteJunqiGame(game[0].name);
@@ -48,4 +109,4 @@ const deleteJunqiGame = async (gameName : string) => {
     return JunqiGame.deleteMany({ name: gameName });
 };
 
-export { createJunqiGame, deletePlayerFromJunqiGame, deleteJunqiGame };
+export { createJunqiGame, setReadyJunqiGame, addPlayerToJunqiGame, startJunqiGame, updateJunqiGame, deletePlayerFromJunqiGame, deleteJunqiGame };
